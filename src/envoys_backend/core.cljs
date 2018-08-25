a60e000965086783f2d244afea1a11cde31d1436
Alex Lynham <acelynham@hotmail.com>
2018-08-25 12:56:23 +0100

    Add body content for endpoints.

--- src/envoys_backend/core.cljs
+++ src/envoys_backend/core.cljs
@@ -17,37 +17,16 @@
       (merge {:title "Envoys | Software Engineering"})))
 
 (def index-data
-  {:hero-text "A functional-first engineering collective with a focus on doing things right, first time."
-   :body-text "We specialise in green-field and rapid prototyping work, from front-end and static lean MVPs to full-stack proof of concepts. Whether it's engineering, devops or business development, we can help with all areas of the project build and lifecycle."
-   :services-list ["Full-stack software engineering"
-                   "Serverless applications"
-                   "Data engineering"
-                   "Prototyping and MVPs"
-                   "Delivery support"
-                   "Architecture and design"
-                   "User engagement and research"
-                   "Workshops and training"]
-   :technologies ["Clojure"
-                  "Javascript"
-                  "Ruby"
-                  "Docker"
-                  "Kubernetes"
-                  "AWS"
-                  "GCP"
-                  "Apache Kafka"
-                  "Apache Spark"
-                  "Neo4j"
-                  "MongoDB"
-                  "PostgreSQL"
-                  "Node.js"
-                  "DAT"
-                  "Blockchain"
-                  "Linked data"
-                  "GraphQL"]})
+  {:hero-text "A functional-first engineering collective that does things right, first time."})
 
 (def about-data
-  {:hero-text "About"
-   :body-text "Lorem Ipsum"})
+  {:hero-text "We specialise in green-field and rapid prototyping, from static MVPs to full-stack apps and data systems. Whether it's engineering, devops or business development, we can help with all areas of the project lifecycle."})
+
+(def blog-data
+  {:hero-text "Under construction..."})
+
+(def contact-data
+  {:hero-text "For general queries, drop us a line at hello@envoys.io. To discuss a project with our CTO, email alex@lynh.am"})
 
 (defn body->layout-as-json [data-map]
   (let [js-map (-> data-map
